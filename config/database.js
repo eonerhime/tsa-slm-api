4421761815565f2f7fa35ceeccfd81665aee725c
Emo Onerhime <emo.onerhime@gmail.com>
2026-03-26 21:02:58 +0100

    feat: implement database connection using mongoose

---
+++ config/database.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    // await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_LOCAL_URI);
+    console.log("Connected to MongoDB");
+    console.log("Database:", mongoose.connection.name);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with an error code
+  }
+};
+
+module.exports = connectDB;
